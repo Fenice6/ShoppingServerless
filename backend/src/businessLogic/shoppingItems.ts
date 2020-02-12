6d2f17eb8b499b16989f3eededbc3d7ff35d30bb
Francesco Lapiana <fenice-6@libero.it>
2020-02-12 12:20:46 +0100

    [Edit] Business and DataLayer
    Created basic structure

---
+++ backend/src/businessLogic/shoppingItems.ts
@@ -0,0 +1,6 @@
+import * as uuid from 'uuid'
+
+import { ShoppingItem } from '../models/ShoppingItem'
+import { ShoppingAccess } from '../dataLayer/shoppingAccess'
+
+const shoppingAccess = new ShoppingAccess() //to access datas
