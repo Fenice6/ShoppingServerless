4ea9dfe1c74c15e5e81489531a8ddc2e75f86e1d
Francesco Lapiana <fenice-6@libero.it>
2020-02-19 11:18:25 +0100

    [Add] Api: getShoppingItemsOfUser.
    This function is used to call backend api to obtain all items of a specific user.

--- client/src/api/shopping-api.ts
+++ client/src/api/shopping-api.ts
@@ -13,3 +13,16 @@ export async function getVisibleShoppingItems(idToken: string): Promise<Shopping
   console.log('ShoppingItems:', response.data)
   return response.data.items
 }
+
+export async function getShoppingItemsOfUser(idToken: string): Promise<ShoppingItem[]> {
+  console.log('Fetching visible shopping items')
+
+  const response = await Axios.get(`${apiEndpoint}/shoppingItemsOfUser`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
+    },
+  })
+  console.log('ShoppingItems:', response.data)
+  return response.data.items
+}
